99800f3521487ec519cfb70c8220bb38e248dc1b
SagedaJaffrenSK <sagedajaffren786@gmail.com>
2025-12-04 10:57:58 +0530

    My second commit

--- async_awit.js
+++ async_awit.js
@@ -28,8 +28,8 @@ const myFun = async () => {
     const result = await getUserDetails("145");
     console.log(result);
 
-    const userSub = await getUserSubjects(result);
-    console.log(userSub);
+    const userSubject = await getUserSubjects(result);
+    console.log(userSubject);
 
     const userMarksDetails = await getUserMarks(userSub);
     console.log(userMarksDetails);
