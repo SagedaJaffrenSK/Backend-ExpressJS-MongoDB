8faf9f70693d58729862c0f9f4dd5597643bc99d
SagedaJaffrenSK <sagedajaffren786@gmail.com>
2025-12-05 11:52:54 +0530

    Commit package-lock

---
+++ server_creation_init.js
@@ -0,0 +1,19 @@
+import http from 'http';
+
+//creation of a server
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    if(req.url === "/users"){
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({name: "Jaffren"}));
+    }
+    else{
+        res.writeHead(200, {"Content-Type" : "text/plain"});
+        res.end("Hello this is from backend");
+    } 
+});
+
+//start a server
+server.listen(7007, () => {
+    console.log(`Server running at port ${7007}`);
+});
